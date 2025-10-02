ed081f2516c262e3cfa926642e42d513b310079a
AaronWizard <aaronjmacd86@gmail.com>
2025-10-02 01:45:36 -0400

    Use file dialog to select files

--- WPFAsyncOperationDemo/Views/FileUploaderView.xaml.cs
+++ WPFAsyncOperationDemo/Views/FileUploaderView.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using FileUploaderDemo.ViewModels;
+using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FileUploaderDemo
 {
@@ -19,6 +12,19 @@ namespace FileUploaderDemo
         public FileUploaderView()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            var fileUploader = DataContext as FileUploaderViewModel;
+            if (fileUploader != null)
+            {
+                var dialog = new OpenFileDialog();
+                if (dialog.ShowDialog() == true)
+                {
+                    fileUploader.FilePath = dialog.FileName;
+                }
+            }
         }
     }
 }
